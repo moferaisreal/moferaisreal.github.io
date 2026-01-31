dadc1aaeec00c68fa36da43d82b54c688d61f9a2
moferaisreal <marcelovprm@gmail.com>
2026-01-30 22:55:10 -0300

    chore:update

--- portfolio/src/components/common/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { StyledButton } from './styles';
-import { ButtonProps } from './types';
-
-export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
-  return <StyledButton {...props}>{children}</StyledButton>;
-};
--- portfolio/src/components/common/Button/types.ts
+++
@@ -1,9 +0,0 @@
-export type ButtonVariant = 'primary' | 'secondary' | 'ghost';
-export type ButtonSize = 'small' | 'medium' | 'large';
-
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: ButtonVariant;
-  size?: ButtonSize;
-  fullWidth?: boolean;
-  isActive?: boolean;
-}
